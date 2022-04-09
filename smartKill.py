72c5da006d12bd078481be48ea2041ff5d3a62d1
pgaillard <paul.frassongaillard@gmail.com>
2022-04-09 15:25:17 +0200

    Adds smartKill script

---
+++ smartKill.py
@@ -0,0 +1,19 @@
+import psutil
+import argparse
+
+def smartKill(name):
+	pids = psutil.pids()
+
+	for p in pids:
+		process = psutil.Process(p)
+	 	if (name in process.name()):
+		#	process.terminate()
+			print(processs.name())
+			print(p)
+
+if __name__ == "main":
+	parser = argparse.ArgumentParser()
+	parser.add_argument("name", type=str, help="Name of the processes to kill")
+	args = parser.parse_args()
+
+	smartKill(args.name)
